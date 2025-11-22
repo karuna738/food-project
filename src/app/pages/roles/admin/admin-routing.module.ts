f7839c3ce0c0a7d52feb32352ff7627b16dc6762
karuna738 <karankarna738@gmail.com>
2025-11-22 22:38:36 +0530

    routing setting

--- src/app/pages/roles/admin/admin-routing.module.ts
+++ src/app/pages/roles/admin/admin-routing.module.ts
@@ -1,12 +1,13 @@
 import { Routes } from "@angular/router";
 import { AdminDashboardComponent } from "./admin-dashboard/admin-dashboard.component";
-import { AdminSettingsComponent } from "./admin-settings/admin-settings.component";
-import { AdminEventComponent } from "./admin-event/admin-event.component";
 
 export const adminRouting: Routes = [
     {path:'', redirectTo:'dashboard', pathMatch:"full"},
     {path:'dashboard', component: AdminDashboardComponent},
     {path: 'producer', loadChildren: () => import('../admin/admin-producer/admin-producer-routing.modules').then(m => m.adminProducerRouting)},
-    {path:'settings', component: AdminSettingsComponent},
-    {path:'event', component: AdminEventComponent},
+    {path: 'processor', loadChildren: () => import('../admin/admin-processor/admin-processor-routing.modules').then(m => m.adminProcessorRouting)},
+    {path: 'distributer', loadChildren: () => import('../admin/admin-distributer/admin-distributer-routing.modules').then(m => m.adminDistributerRouting)},
+    {path: 'retailer', loadChildren: () => import('../admin/admin-retailer/admin-retailer-routing.modules').then(m => m.adminRetailerRouting)},
+    {path: 'settings', loadChildren: () => import('../admin/admin-settings/admin-settings-routing.modules').then(m => m.adminSettingsRouting)},
+    {path: 'event', loadChildren: () => import('../admin/admin-event/admin-event-routing.modules').then(m => m.adminEventRouting)},
 ]
